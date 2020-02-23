7c1fad74d1ecd8a32b1e2115d2225517b7b6fec4
Ashish Gaikwad <ash.gkwd@gmail.com>
2020-02-23 15:27:53 +0530

    adds layout for jur status screen's flows

--- front/src/ui/screens/JurStatusScreen.js
+++ front/src/ui/screens/JurStatusScreen.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Card, Row, Col } from "antd";
+
+function CreateJurType() {
+  return <Card title="Create Jur Type"></Card>;
+}
+
+function CreateJur() {
+  return <Card title="Create Jur"></Card>;
+}
+
+function JurList() {
+  return <Card title="Your Jurs"></Card>;
+}
 
 function JurStatusScreen() {
-  return <div>This is jur status screen</div>;
+  return (
+    <Row type="flex" justify="space-around">
+      <Col span={8}>
+        <CreateJurType />
+      </Col>
+      <Col span={6}>
+        <CreateJur />
+      </Col>
+      <Col span={6}>
+        <JurList />
+      </Col>
+    </Row>
+  );
 }
 
 export default JurStatusScreen;
